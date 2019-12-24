f88a8326d07fbcad9201c7922ae2937a78aafd95
Danny Lee <dreamer@panix.com>
2019-12-24 10:02:39 -0500

    Change Search.js to class component and add redirect if not logged in.

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 
-const Search = props => (
-  <div>
-    <h1>Search</h1>
-  </div>
-)
+class Search extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      myState: true
+    }
+  }
+
+  render () {
+    if (!this.props.state.loggedIn) {
+      return <Redirect to='/profile' />
+    } else {
+      return (
+        <div>
+          <h1>Hello Search</h1>
+        </div>
+      )
+    }
+  }
+}
 
 export default Search
+
