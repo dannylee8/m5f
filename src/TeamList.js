e809abba39d6c06b466b8c9f785771b5a6a83c6d
Danny Lee <dreamer@panix.com>
2019-12-28 12:23:57 -0500

    Change Teamlist.js from class to functional in previous commit

--- src/TeamList.js
+++ src/TeamList.js
@@ -1,44 +1,31 @@
-import React, { Component } from 'react'
-import Tooltip from '@material-ui/core/Tooltip';
+import React from 'react'
+import Tooltip from '@material-ui/core/Tooltip'
 
 const uuidv4 = require('uuid/v4')
 
-class TeamList extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      // myState: true
-    }
-  }
-
-  onClickHandler = (team) => { this.props.selectTeam(team) }
-
-  render () {
-    return (
-      <>
-        {this.props.state.current_user_teams.map(team => {
-          if (team.name.trim().toLowerCase() === this.props.state.teamSelected.trim().toLowerCase() || this.props.state.teamSelected === '') {
-            return (
-              <React.Fragment key={uuidv4()}>
-                <tr key={uuidv4()} >
-                  <td key={team.id} className='team-list' onClick={e => this.onClickHandler(team)}> 
-                    { team.name } 
-                  </td>
-                  <td key={uuidv4()}>
-                    <Tooltip title="Delete">
-                        <i onClick={()=>this.props.handleDeleteTeam(team)} className='material-icons-outlined'>delete</i>
-                    </Tooltip>
-                  </td>
-                </tr>
-              </React.Fragment>
-            )
-          } else {
-            return null
-          }
-        })}
-      </>
-    )
-  }
-}
+const TeamList = props => (
+  <>
+    {props.state.current_user_teams.map(team => {
+      if (team.name.trim().toLowerCase() === props.state.teamSelected.trim().toLowerCase() || props.state.teamSelected === '') {
+        return (
+          <React.Fragment key={uuidv4()}>
+            <tr>
+              <td key={team.id} className='team-list' onClick={() => props.selectTeam(team)}>
+                {team.name}
+              </td>
+              <td>
+                <Tooltip title='Delete'>
+                  <i onClick={() => props.handleDeleteTeam(team)} className='material-icons-outlined'>delete</i>
+                </Tooltip>
+              </td>
+            </tr>
+          </React.Fragment>
+        )
+      } else {
+        return null
+      }
+    })}
+  </>
+)
 
 export default TeamList
