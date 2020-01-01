32449c231166c8d2ec10514d306cde8ba1c65321
Danny Lee <dreamer@panix.com>
2019-12-31 23:21:30 -0500

    Wasted time trying to create modal.  Reset to working position

--- src/Teams.js
+++ src/Teams.js
@@ -7,22 +7,22 @@ const uuidv4 = require('uuid/v4')
 
 class Teams extends Component {
 
-  alphabetizedList () {
-    const teamPos = this.props.findPositionsOnTeam(this.props.state.teamObject.id).sort((x, y) => {
-      const a = this.props.findUserByID(x.user_id)
-      const b = this.props.findUserByID(y.user_id)
-      if (a === b) {
-        return 0
-      } else if (a === undefined) {
-        return 1
-      } else if (b === undefined) {
-        return -1
-      } else {
-        return a.name > b.name ? 1 : -1
-      }
-    })
-    return teamPos.map(tp => this.props.findUserByID(tp.user_id))
-  }
+  // alphabetizedList () {
+  //   const teamPos = this.props.findPositionsOnTeam(this.props.state.teamObject.id).sort((x, y) => {
+  //     const a = this.props.findUserByID(x.user_id)
+  //     const b = this.props.findUserByID(y.user_id)
+  //     if (a === b) {
+  //       return 0
+  //     } else if (a === undefined) {
+  //       return 1
+  //     } else if (b === undefined) {
+  //       return -1
+  //     } else {
+  //       return a.name > b.name ? 1 : -1
+  //     }
+  //   })
+  //   return teamPos.map(tp => this.props.findUserByID(tp.user_id))
+  // }
 
   render () {
     if (!this.props.state.loggedIn) {
@@ -86,8 +86,16 @@ class Teams extends Component {
                           state={this.props.state}
                           singlePosition={p}
                           findUserByID={this.props.findUserByID}
-                        />
-                    )}
+                        />)
+                        
+                    }
+                    <tr className='centered'>
+                      <td>+</td>
+                      <td>
+                        <button>Add Position</button>
+                      </td>
+                      <td>+</td>
+                    </tr>
                   </tbody>
                 </table>
                 <div className='team-right-side'>
