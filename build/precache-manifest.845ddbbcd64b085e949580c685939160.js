1efd7aa52f8a351cc74b24852d299c77e7b6d79d
Danny Lee <dreamer@panix.com>
2020-01-06 17:11:07 -0500

    added server.js

---
+++ build/precache-manifest.845ddbbcd64b085e949580c685939160.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d79d8587732cefc65bb669a3eb69e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24799ac5dbb346a22db4",
+    "url": "/static/css/main.d5e9c777.chunk.css"
+  },
+  {
+    "revision": "4685dda8726b181e9699",
+    "url": "/static/js/2.7151f8ec.chunk.js"
+  },
+  {
+    "revision": "ed68e69a2117aa0b088bf77d21506941",
+    "url": "/static/js/2.7151f8ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "24799ac5dbb346a22db4",
+    "url": "/static/js/main.b144e16b.chunk.js"
+  },
+  {
+    "revision": "d6084c41f2518b2e3626",
+    "url": "/static/js/runtime-main.cc4b70fc.js"
+  },
+  {
+    "revision": "794d3835b08c79c30cae1f4a9ee2c2b0",
+    "url": "/static/media/mooning.794d3835.png"
+  },
+  {
+    "revision": "41afc6c2bc067054a93d154dade75bc6",
+    "url": "/static/media/subtle_white_feathers.41afc6c2.png"
+  }
+]);
