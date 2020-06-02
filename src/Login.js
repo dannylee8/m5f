102ad2390cc963e0c2a2906408fbb2b84dfc20b2
Danny Lee <dreamer@panix.com>
2020-06-02 00:52:09 -0400

    update

--- src/Login.js
+++ src/Login.js
@@ -1,171 +1,203 @@
-import React, { Component } from 'react'
-import { Button, Container, Form, FormGroup, Label, Input, Fade } from 'reactstrap'
-import { Redirect } from 'react-router-dom'
-import _ from 'lodash'
+import React, { Component } from "react";
+import {
+  Button,
+  Container,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Fade,
+} from "reactstrap";
+import { Redirect } from "react-router-dom";
+import _ from "lodash";
 
 const validationMethods = {
   required: (field, value) => {
     if (!value.toString().trim().length) {
-      return `This ${field} field is required.`
+      return `This ${field} field is required.`;
     }
   },
-  isEmail: (field,value) => {
+  isEmail: (field, value) => {
     // eslint-disable-next-line
     var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
     if (reg.test(value) === false) {
-      return 'Invalid Email Address.'
+      return "Invalid Email Address.";
     }
-  }
-}
+  },
+};
 
 const validateForm = (form) => {
-  const loginForm = document.getElementById(form)
-  return loginForm.querySelectorAll('[validations]')
-}
+  const loginForm = document.getElementById(form);
+  return loginForm.querySelectorAll("[validations]");
+};
 
 const runValidationRules = (element, errors) => {
-  const target = element
-  const field = target.name
-  const value = target.value
-  let validations = element.getAttribute('validations')
-  validations = validations.split(',')
+  const target = element;
+  const field = target.name;
+  const value = target.value;
+  let validations = element.getAttribute("validations");
+  validations = validations.split(",");
 
   for (let validation of validations) {
-    validation = validation.split(':')
-    const rule = validation[0]
-    const error = validationMethods[rule](field, value)
-    errors[field] = errors[field] || {}
+    validation = validation.split(":");
+    const rule = validation[0];
+    const error = validationMethods[rule](field, value);
+    errors[field] = errors[field] || {};
     if (error) {
-      errors[field][rule] = error
+      errors[field][rule] = error;
     } else {
       if (_.isEmpty(errors[field])) {
-        delete errors[field]
+        delete errors[field];
       } else {
-        delete errors[field][rule]
+        delete errors[field][rule];
       }
     }
   }
-  return errors
-}
+  return errors;
+};
 
 export default class Login extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
       errors: [],
-      redirect: false
-    }
+      redirect: false,
+    };
   }
 
   setRedirect = () => {
     this.setState({
-      redirect: true
-    })
-  }
+      redirect: true,
+    });
+  };
 
   renderRedirect = () => {
     if (this.state.redirect) {
-      return <Redirect to='/new_user' />
+      return <Redirect to="/new_user" />;
     }
-  }
+  };
 
-    login = (event) => {
+  login = (event) => {
+    event.preventDefault();
 
-      event.preventDefault();
+    const formElements = validateForm("loginForm");
 
-      const formElements = validateForm('loginForm');
+    formElements.forEach((element) => {
+      const errors = runValidationRules(element, this.state.errors);
+      this.setState({
+        errors: errors,
+      });
+    });
 
-      formElements.forEach(element=> {
-        const errors = runValidationRules(element, this.state.errors);
-        this.setState({
-          errors: errors
-        });
-      })
-
-      const email = this.state.email
-      const password = this.state.password
-      const errors = this.state.errors
-      if (!this.state.errors.email && !this.state.errors.password) {
-        if (this.props.logthemin(email)) {
-          return true
-        } else {
-          alert('No such user!')
-          this.setState({
-            email: '',
-            password: ''
-          })
-        }
+    const email = this.state.email;
+    const password = this.state.password;
+    const errors = this.state.errors;
+    if (!this.state.errors.email && !this.state.errors.password) {
+      if (this.props.logthemin(email)) {
+        return true;
       } else {
-        console.log(email, password, errors)
+        alert("No such user!");
+        this.setState({
+          email: "",
+          password: "",
+        });
       }
+    } else {
+      console.log(email, password, errors);
     }
+  };
 
   handleChange = (event) => {
-    const target = event.target
-    const field = target.name
-    const value = target.value
+    const target = event.target;
+    const field = target.name;
+    const value = target.value;
 
-    const errors = runValidationRules(target, this.state.errors)
+    const errors = runValidationRules(target, this.state.errors);
     this.setState({
-      errors: errors
-    })
+      errors: errors,
+    });
 
     this.setState({
-      [field]: value
-    })
-  }
+      [field]: value,
+    });
+  };
 
-  render () {
+  render() {
     return (
       <>
         {this.renderRedirect()}
-        <Container className='login-container'>
-          <Form id='loginForm' method='post' onSubmit={this.login}>
+        <Container className="login-container">
+          <Form id="loginForm" method="post" onSubmit={this.login}>
             <FormGroup>
-              <div className='login-row'>
+              <div className="login-row">
                 <Label>Email</Label>
-                <Input 
-                  className='input'
-                  type='text'
-                  validations={['required','isEmail']}
-                  name='email'
+                <Input
+                  className="input"
+                  type="text"
+                  validations={["required", "isEmail"]}
+                  name="email"
                   value={this.state.email}
                   onChange={this.handleChange}
-                  id='email'
-                  placeholder=''
+                  id="email"
+                  placeholder=""
                 />
               </div>
-              <div className='login-row'>
+              <div className="login-row">
                 <Label>Password</Label>
                 <Input
-                  className='input'
-                  type='password'
-                  validations={['required']}
-                  name='password'
+                  className="input"
+                  type="password"
+                  validations={["required"]}
+                  name="password"
                   value={this.state.password}
                   onChange={this.handleChange}
-                  id='password'
-                  placeholder=''
+                  id="password"
+                  placeholder=""
                 />
               </div>
+              <div>
+                <li>
+                  <a href="https://github.com/dannylee8/m5f">
+                    GitHub Frontend repo
+                  </a>
+                </li>
+                <li>
+                  <a href="https://github.com/dannylee8/m5b">
+                    GitHub Backend repo
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.youtube.com/watch?v=_rki3cc7K0g">
+                    Video walkthru at YouTube
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.panix.com/~dreamer/">
+                    Visit my homepage
+                  </a>
+                </li>
+              </div>
             </FormGroup>
-            <Button onClick={this.login} color='secondary'>Login</Button>
-            <div className='divider'/>
-            <Button onClick={this.setRedirect} color='secondary'>New User</Button>
+            <Button onClick={this.login} color="secondary">
+              Login
+            </Button>
+            <div className="divider" />
+            <Button onClick={this.setRedirect} color="secondary">
+              New User
+            </Button>
           </Form>
         </Container>
         <FromValidationError field={this.state.errors.email} />
         <FromValidationError field={this.state.errors.password} />
       </>
-
-    )
+    );
   }
 }
- 
-const FromValidationError = props => (
-  <Fade in={Boolean(props.field)} tag='p' className='error centered'>
-    {props.field ? Object.values(props.field).shift() : '' } 
+
+const FromValidationError = (props) => (
+  <Fade in={Boolean(props.field)} tag="p" className="error centered">
+    {props.field ? Object.values(props.field).shift() : ""}
   </Fade>
-)
+);
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -13,36 +13,17 @@ export default function Sidebar(props) {
         onHandleDelete={props.handleDelete}
       />
       {props.cUser ? null : (
-        <div className="sample-info">
-          <ul>
-            <li>
-              <b>Sample Users:</b>
-            </li>
-            <li>thanh@gmail.com</li>
-            <li>jenine@gmail.com</li>
-            <li>marcus@gmail.com</li>
-            <li>yang@gmail.com</li>
-            <li>dorian@gmail.com</li>
-            <br />
-            <li>Password: password</li>
-            <li>
-              <a href="https://github.com/dannylee8/m5f">
-                GitHub Frontend repo
-              </a>
-            </li>
-            <li>
-              <a href="https://github.com/dannylee8/m5b">GitHub Backend repo</a>
-            </li>
-            <li>
-              <a href="https://www.youtube.com/watch?v=_rki3cc7K0g">
-                Video walkthru at YouTube
-              </a>
-            </li>
-            <li>
-              <a href="https://www.panix.com/~dreamer/">Visit my homepage</a>
-            </li>
-          </ul>
-        </div>
+        <ul className="sample-info">
+          <li>
+            <b>Sample Users:</b>
+          </li>
+          <li>thanh@gmail.com</li>
+          <li>jenine@gmail.com</li>
+          <li>marcus@gmail.com</li>
+          <li>yang@gmail.com</li>
+          <li>dorian@gmail.com</li>
+          <li>Password: password</li>
+        </ul>
       )}
     </div>
   );
