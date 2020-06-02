87e0cbab6a9942cc30857cf060c2a0657136eab5
Danny Lee <dreamer@panix.com>
2020-06-02 00:29:11 -0400

    update

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,38 +1,43 @@
-import React from 'react'
-import CurrentUserName from './CurrentUserName'
-import NavMenu from './NavMenu'
+import React from "react";
+import CurrentUserName from "./CurrentUserName";
+import NavMenu from "./NavMenu";
 
-export default function Sidebar (props) {
+export default function Sidebar(props) {
   return (
-    <div className='sidenav'>
+    <div className="sidenav">
       <CurrentUserName cUser={props.cUser} />
-      <NavMenu 
-        cUser={props.cUser} 
-        goBackHandler={props.goBackHandler} 
-        onHandleLogout={props.handleLogout} 
-        onHandleDelete={props.handleDelete} />
-      {props.cUser ? null : 
-        <ul className='sample-info'>
-          <li><b>Sample Users:</b></li>
-          <li>thanh@gmail.com</li>
-          <li>jenine@gmail.com</li>
-          <li>marcus@gmail.com</li>
-          <li>yang@gmail.com</li>
-          <li>dorian@gmail.com</li>
-        </ul>
-        <p>
-            Password: password
-        </p>
-        <p>
+      <NavMenu
+        cUser={props.cUser}
+        goBackHandler={props.goBackHandler}
+        onHandleLogout={props.handleLogout}
+        onHandleDelete={props.handleDelete}
+      />
+      {props.cUser ? null : (
+        <>
+          <ul className="sample-info">
+            <li>
+              <b>Sample Users:</b>
+            </li>
+            <li>thanh@gmail.com</li>
+            <li>jenine@gmail.com</li>
+            <li>marcus@gmail.com</li>
+            <li>yang@gmail.com</li>
+            <li>dorian@gmail.com</li>
+          </ul>
+          <p>Password: password</p>
+          <p>
             <a href="https://github.com/dannylee8/m5f">GitHub Frontend repo</a>
             <br />
             <a href="https://github.com/dannylee8/m5b">GitHub Backend repo</a>
             <br />
-            <a href="https://www.youtube.com/watch?v=_rki3cc7K0g">Video walkthru at YouTube</a>
+            <a href="https://www.youtube.com/watch?v=_rki3cc7K0g">
+              Video walkthru at YouTube
+            </a>
             <br />
             <a href="https://www.panix.com/~dreamer/">Visit my homepage</a>
-        </p>
-      }
+          </p>
+        </>
+      )}
     </div>
-  )
+  );
 }
